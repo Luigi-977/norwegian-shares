137d74331c850dd7ff94686b2adabd7da1c2aca0
Luigi-977 <machokibryson88@gmail.com>
2026-05-20 10:36:08 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdweu2Y8ZSfilJsh5g0PKPwwiE7GXxoSk",
+  authDomain: "norwegian-shares.firebaseapp.com",
+  projectId: "norwegian-shares",
+  storageBucket: "norwegian-shares.firebasestorage.app",
+  messagingSenderId: "43355796470",
+  appId: "1:43355796470:web:afa61eb967b0beecc68dd7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+await setPersistence(auth, browserLocalPersistence);
+
+export { auth, db };
