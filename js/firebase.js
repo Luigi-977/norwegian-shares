30d1c1a50376d0d155b6414b6987476e7596f63d
Luigi-977 <machokibryson88@gmail.com>
2026-05-21 22:26:41 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,23 @@
+// FIREBASE CONFIG
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 
- // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAT-Aou7qeoCkMYR6TXrZ5cPjkCWA4aGSQ",
-    authDomain: "luigiblvck.firebaseapp.com",
-    projectId: "luigiblvck",
-    storageBucket: "luigiblvck.firebasestorage.app",
-    messagingSenderId: "749834936416",
-    appId: "1:749834936416:web:0829759868cfd1082c6305"
-  };
+// YOUR FIREBASE CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyAT-Aou7qeoCkMYR6TXrZ5cPjkCWA4aGSQ",
+  authDomain: "luigiblvck.firebaseapp.com",
+  projectId: "luigiblvck",
+  storageBucket: "luigiblvck.firebasestorage.app",
+  messagingSenderId: "749834936416",
+  appId: "1:749834936416:web:0829759868cfd1082c6305"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// INITIALIZE FIREBASE
+const app = initializeApp(firebaseConfig);
+
+// AUTH
+const auth = getAuth(app);
+
+// EXPORT
+export { auth };
