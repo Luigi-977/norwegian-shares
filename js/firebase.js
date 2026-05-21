3a0803b2e6c97424f47cd38fe7de755d959d4ecf
Luigi-977 <machokibryson88@gmail.com>
2026-05-21 23:27:17 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,30 +1,16 @@
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 
-import { getAuth }
-from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 
-// FIREBASE CONFIG
 const firebaseConfig = {
-
   apiKey: "AIzaSyAT-Aou7qeoCkMYR6TXrZ5cPjkCWA4aGSQ",
-
   authDomain: "luigiblvck.firebaseapp.com",
-
   projectId: "luigiblvck",
-
   storageBucket: "luigiblvck.firebasestorage.app",
-
   messagingSenderId: "749834936416",
-
   appId: "1:749834936416:web:0829759868cfd1082c6305"
 };
 
-// INITIALIZE
 const app = initializeApp(firebaseConfig);
 
-// AUTH
-const auth = getAuth(app);
-
-// EXPORT
-export { auth };
+export const auth = getAuth(app);
