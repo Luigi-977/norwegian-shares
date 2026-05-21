f5377372506290195d09d6382dc370e8fc0de59e
Luigi-977 <machokibryson88@gmail.com>
2026-05-21 23:13:15 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,47 +1,52 @@
-import { auth } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
 import {
+  getAuth,
   signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// FORM
-const loginForm = document.getElementById("loginForm");
+const firebaseConfig = {
+  apiKey: "AIzaSyAT-Aou7qeoCkMYR6TXrZ5cPjkCWA4aGSQ",
+  authDomain: "luigiblvck.firebaseapp.com",
+  projectId: "luigiblvck",
+  storageBucket: "luigiblvck.firebasestorage.app",
+  messagingSenderId: "749834936416",
+  appId: "1:749834936416:web:0829759868cfd1082c6305"
+};
 
-// SUBMIT EVENT
-loginForm.addEventListener("submit", async (event) => {
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", async (event) => {
 
   event.preventDefault();
 
-  // INPUT VALUES
   const email =
-    document.getElementById("loginEmail").value.trim();
+    document.getElementById("email").value;
 
   const password =
-    document.getElementById("loginPassword").value;
-
-  // VALIDATION
-  if (!email || !password) {
-    alert("Please fill all fields");
-    return;
-  }
+    document.getElementById("password").value;
 
   try {
 
-    // LOGIN USER
     await signInWithEmailAndPassword(
       auth,
       email,
       password
     );
 
-    alert("Login successful!");
+    alert("Login successful");
 
-    // REDIRECT
     window.location.href = "dashboard.html";
 
   } catch (error) {
 
-    alert(error.message);
+    document.getElementById("message").innerHTML =
+      error.message;
+
   }
 
 });
