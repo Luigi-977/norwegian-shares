eff22392bcbd74ac7c048b52b8673192db19e711
Luigi-977 <machokibryson88@gmail.com>
2026-05-21 09:43:50 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,18 @@
-  // Import the functions you need from the SDKs you need
+
+ // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyAdweu2Y8ZSfilJsh5g0PKPwwiE7GXxoSk",
-    authDomain: "norwegian-shares.firebaseapp.com",
-    projectId: "norwegian-shares",
-    storageBucket: "norwegian-shares.firebasestorage.app",
-    messagingSenderId: "43355796470",
-    appId: "1:43355796470:web:b4c84016620f7d1ac68dd7",
-    measurementId: "G-ZTMV5LN4WY"
+    apiKey: "AIzaSyAT-Aou7qeoCkMYR6TXrZ5cPjkCWA4aGSQ",
+    authDomain: "luigiblvck.firebaseapp.com",
+    projectId: "luigiblvck",
+    storageBucket: "luigiblvck.firebasestorage.app",
+    messagingSenderId: "749834936416",
+    appId: "1:749834936416:web:0829759868cfd1082c6305"
   };
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
